833bb7e074b9c6f040ec3189376484a7f6f289ee
Mike Belanger <mikebelanger@fastmail.com>
2016-10-27 14:19:32 -0400

    All layout now in .cljs, still have to convert stuff to defelm.  Beginnings of basic page layout

--- src/cljs/ui.cljs
+++ src/cljs/ui.cljs
@@ -5,3 +5,20 @@
 
 (defn height []
   (.-innerHeight js/window))
+
+(defn equally-among-elem [elem]
+  "Return the share of each element's share of dimensions, based on size of elem."
+  (str (/ 100 (count elem)) "%"))
+
+(def basic-style {:font-family "Andale Mono"
+                  :margin "0px"
+                  :padding "0px"})
+
+(def flex-shims {:display "flex"
+                 :flex-wrap "wrap"
+                 :flex-direction "row"})
+
+(def basic-flex-row-style (conj flex-shims
+                            {:min-height "50px"
+                             :margin "0px"}
+                                basic-style))
