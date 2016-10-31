7c4900a390d926346b042abd51dc36709bb1f965
Mike Belanger <mikebelanger@fastmail.com>
2016-10-30 22:03:59 -0400

    better css - god I hate css though

--- src/cljs/ui.cljs
+++ src/cljs/ui.cljs
@@ -1,12 +1,14 @@
 (ns cljs.ui)
 
-(defn equally-among-elem [elem]
+(defn equally-among-elem [elem margin]
   "Return the share of each element's share of dimensions, based on size of elem."
   (str (/ 100 (count elem)) "%"))
 
-(def basic-flex-row-style {:font-family "Andale Mono"
-                           :width "100%"
-                           :display "flex"
-                           :flex-wrap "wrap"
-                           :flex-direction "row"
-                           :min-height "50px"})
+(def responsive-row {:font-family "Andale Mono"
+                     :width "100%"
+                     :display "table"
+                     :float "left"})
+
+(def box {:display "table-cell"
+          :text-align "center"
+          :vertical-align "middle"})
